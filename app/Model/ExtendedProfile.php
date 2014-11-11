f460e46020b5c2d6b8a9789374d6a1c01750ee11
Pete Olsen <hepaestus@gmail.com>
2014-11-11 11:00:28 -0500

    Removed gender and marriage status validation, changes tabs to spaces.

--- app/Model/ExtendedProfile.php
+++ app/Model/ExtendedProfile.php
@@ -13,140 +13,140 @@ class ExtendedProfile extends AppModel {
  *
  * @var string
  */
-	public $displayField = 'first_name';
+    public $displayField = 'first_name';
 
 /**
  * Validation rules
  *
  * @var array
  */
-	public $validate = array(
-		'scene_name' => array(
-			'maxLength' => array(
-				'rule' => array('maxLength' , 50),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'first_name' => array(
-			'maxLength' => array(
-				'rule' => array('maxLength' , 50),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'last_name' => array(
-			'maxLength' => array(
-				'rule' => array('maxLength', 50),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'postal_code' => array(
-			'postal' => array(
-				'rule' => array('postal'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'birth_date' => array(
-			'date' => array(
-				'rule' => array('date'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'gender_identity' => array(
-			'inList' => array(
-				'rule' => array('inList', array('male','female','none')),
-				//'message' => 'Your custom message here',
-				'allowEmpty' => true,
-				'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'relationship_status' => array(
-			'inList' => array(
-				'rule' => array('inList', array('married','single','complex')),
-				//'message' => 'Your custom message here',
-				'allowEmpty' => true,
-				'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'latitude' => array(
-			'decimal' => array(
-				'rule' => array('decimal'),
-				//'message' => 'Your custom message here',
-				'allowEmpty' => true,
-				'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'longitude' => array(
-			'decimal' => array(
-				'rule' => array('decimal'),
-				//'message' => 'Your custom message here',
-				'allowEmpty' => true,
-				'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'created' => array(
-			'datetime' => array(
-				'rule' => array('datetime'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'modified' => array(
-			'datetime' => array(
-				'rule' => array('datetime'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-	);
+    public $validate = array(
+        'scene_name' => array(
+            'maxLength' => array(
+                'rule' => array('maxLength' , 50),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'first_name' => array(
+            'maxLength' => array(
+                'rule' => array('maxLength' , 50),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'last_name' => array(
+            'maxLength' => array(
+                'rule' => array('maxLength', 50),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'postal_code' => array(
+            'postal' => array(
+                'rule' => array('postal'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'birth_date' => array(
+            'date' => array(
+                'rule' => array('date'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        #'gender_identity' => array(
+        #    'inList' => array(
+        #        'rule' => array('inList', array('male','female','none')),
+        #        //'message' => 'Your custom message here',
+        #        'allowEmpty' => true,
+        #        'required' => false,
+        #        //'last' => false, // Stop validation after this rule
+        #        //'on' => 'create', // Limit validation to 'create' or 'update' operations
+        #    ),
+        #),
+        #'relationship_status' => array(
+        #    'inList' => array(
+        #        'rule' => array('inList', array('married','single','complex')),
+        #        //'message' => 'Your custom message here',
+        #        'allowEmpty' => true,
+        #        'required' => false,
+        #        //'last' => false, // Stop validation after this rule
+        #        //'on' => 'create', // Limit validation to 'create' or 'update' operations
+        #    ),
+        #),
+        'latitude' => array(
+            'decimal' => array(
+                'rule' => array('decimal'),
+                //'message' => 'Your custom message here',
+                'allowEmpty' => true,
+                'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'longitude' => array(
+            'decimal' => array(
+                'rule' => array('decimal'),
+                //'message' => 'Your custom message here',
+                'allowEmpty' => true,
+                'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'created' => array(
+            'datetime' => array(
+                'rule' => array('datetime'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+        'modified' => array(
+            'datetime' => array(
+                'rule' => array('datetime'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
+    );
 
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
+    //The Associations below have been created with all possible keys, those that are not needed can be removed
 
 /**
  * belongsTo associations
  *
  * @var array
  */
-	public $belongsTo = array(
-		'User' => array(
-			'className' => 'User',
-			'foreignKey' => 'user_id',
-			'conditions' => '',
-			'fields' => '',
-			'order' => ''
-		)
-	);
+    public $belongsTo = array(
+        'User' => array(
+            'className' => 'User',
+            'foreignKey' => 'user_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        )
+    );
 }
